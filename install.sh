71159be25ff73d52f799a550b592308a7c7e263d
vagrant <vagrant@scotchbox>
2015-06-24 13:31:19 +0000

    added installation for ubuntu dev env

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo apt-get install ffmpeg libavcodec-extra-*
+
+
+
+sudo add-apt-repository -y ppa:kivy-team/kivy
+sudo apt-get -y update
+
+
+
+
+sudo pip install pyinstaller
+sudo pip install Cython==0.17.1
+sudo pip install hg+http://bitbucket.org/pygame/pygame
+sudo pip install kivy
+
+sudo apt-get -y install kivy-examples
